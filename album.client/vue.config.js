a92ffb9f0d00d9f08017d29395d51b8da754f945
pburkett <pburkett636@gmail.com>
2021-02-18 15:01:51 -0700

    setup vue.config for deployment

--- album.client/vue.config.js
+++ album.client/vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
- // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/',
-  outputDir: 'docs',
+  outputDir: '../curb-side.server/client',
   productionSourceMap: false
 }
