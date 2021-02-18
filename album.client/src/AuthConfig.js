a32986b2b30dea9cbb47a81af2d8bc54374ff30d
pburkett <pburkett636@gmail.com>
2021-02-18 15:29:41 -0700

    authconfig setup

--- album.client/src/AuthConfig.js
+++ album.client/src/AuthConfig.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-jxvfv4ks.us.auth0.com'
+export const clientId = '8aCPMTJersXKmBkKcxBfjiQyr14hT7Fi'
+export const audience = 'https://album.com'
